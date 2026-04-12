b6b8d072b53145760de71d300cbbb79ee4a42140
OueslatiOussema23 <itsjustossema@gmail.com>
2026-04-12 17:42:02 +0200

    pour Atta

---
+++ pfa/Views/Partials/navbar.inc.php
@@ -0,0 +1,27 @@
+ 
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="Home">Madresty</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="contact">Contactez-nous</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            <div>
+                <span class= "d-none d-sm-inline ms-2">
+                <a href="login" class="btn btn-outline-primary">Se connecter</a>
+                </span>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ pfa/plain.inc.php
@@ -0,0 +1,55 @@
+<!-- cette page n'est pas dynamique pour le momment 
+ il faut changer quelques trucs pour la rendre responsive selon le role -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $titre ?? 'Acceuil'; ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+</head>
+<body>
+    <?php require_once 'Views/Partials/navbar.inc.php';?>
+    <div class="container-fluid">
+        <div class="row" style="height: 100vh;">
+            <div class="col-2 col-sm-3 col-xl-2 bg-dark">
+                <div class="container">
+                    <nav class="navbar bg-dark border-bottom border-white mb-3" data-bs-theme="dark">
+                        <div class="container-fluid">
+                            <a href="" class="navbar-brand">Enseignant</a>
+                        </div>
+                    </nav>
+                    <nav class="nav flex-column">
+                        <a class="nav-link text-white" aria-current="" href="/dashboard">
+                            <i class="bi bi-speedometer2"></i><span class="d-none d-sm-inline ms-2">Dashboard</span>
+                        </a>
+                        <a class="nav-link text-white" href="/calendar">
+                            <i class="bi bi-calendar-fill"></i><span class="d-none d-sm-inline ms-2">Calendrier</span>
+                        </a>
+                        <a class="nav-link text-white" href="/pfa/enseignant/ficheAppel">
+                            <i class="bi bi-journal"></i><span class="d-none d-sm-inline ms-2">Feuille d'appel</span>
+                        </a>
+                        <a class="nav-link text-white" href="/pfa/enseignant/ajouterNotes">
+                            <i class="bi bi-pencil-square"></i><span class="d-none d-sm-inline ms-2">Saisir notes</span>
+                        </a>
+                    </nav>
+                </div>
+            </div>
+            <div class="col-10 col-sm-9 col-md-10">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary mb-3">
+                    <div class="container-fluid">
+                        
+                    </div>
+                </nav>
+                <div class="container">
+                    <?php require_once $view . ".php";?>
+                </div>
+                <div class="container">
+                    <?php print_r($_SESSION) ?>
+                </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
